beb834b840d7cfdb5c980ae9c71b6fd39d81c55e
ecoyocena <enricoabbona@gmail.com>
2024-03-17 22:11:12 +0100

    demo update mobile

--- input.js
+++ input.js
@@ -42,3 +42,50 @@ export function getInputDirection() {
   lastInputDirection = inputDirection;
   return inputDirection;
 }
+
+let startX, startY;
+
+window.addEventListener(
+  "touchstart",
+  function (event) {
+    startX = event.touches[0].clientX;
+    startY = event.touches[0].clientY;
+  },
+  false
+);
+
+window.addEventListener(
+  "touchmove",
+  function (event) {
+    let endX = event.touches[0].clientX;
+    let endY = event.touches[0].clientY;
+
+    let deltaX = startX - endX;
+    let deltaY = startY - endY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      // Scorrimento orizzontale
+      if (deltaX > 0) {
+        if (lastInputDirection.x !== 0);
+        inputDirection = { x: -1, y: 0 };
+        console.log("Scorrimento verso sinistra");
+      } else {
+        if (lastInputDirection.x !== 0) ;
+        inputDirection = { x: 1, y: 0 };
+        console.log("Scorrimento verso destra");
+      }
+    } else {
+      // Scorrimento verticale
+      if (deltaY > 0) {
+        if (lastInputDirection.y !== 0) ;
+        inputDirection = { x: 0, y: -1 };
+        console.log("Scorrimento verso l'alto");
+      } else {
+        if (lastInputDirection.y !== 0) ;
+        inputDirection = { x: 0, y: 1 };
+        console.log("Scorrimento verso il basso");
+      }
+    }
+  },
+  false
+);
